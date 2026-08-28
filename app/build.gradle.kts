0ac79d5af097133d88752176eefe019b6b9b9d3d
Boluofan <13588211351@163.com>
2026-08-28 14:59:56 +0800

    chore: release version 2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "top.boluofan.musictv"
         minSdk = 21
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.0.1"
+        versionCode = 12
+        versionName = "2.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         aaptOptions {
             ignoreAssetsPattern = "!.svn:!.git:!.ds_store:!*.scc:.*:!CVS:!thumbs.db:!picasa.ini:!*~"
